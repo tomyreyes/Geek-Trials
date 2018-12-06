ac522747323286bae30d7ac7fd70bdaa206bb00e
Tomy <tomyleereyes@gmail.com>
2018-12-05 19:44:30 -0800

    add back-end and frontend directories

---
+++ front-end/routes/api.js
@@ -0,0 +1,13 @@
+const express = require(express)
+const router = express.Router()
+const axios = require('axios')
+
+router.get('/get-questions', (req, res) => {
+  const { category, difficulty } = req.body
+  return axios
+    .get(
+      `https://opentdb.com/api.php?amount=10&category=${category}&type=multiple&difficulty=${difficulty}`
+    )
+    .then(results => res.json(results.data.results))
+    .catch(error => res.json(error))
+})
