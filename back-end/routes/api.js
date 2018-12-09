5cb19f87d4c56f3028415bcb42690aee9d653f8a
Tomy <tomyleereyes@gmail.com>
2018-12-09 10:08:32 -0800

    fix route to return early

--- back-end/routes/api.js
+++ back-end/routes/api.js
@@ -1,12 +1,15 @@
-const express = require(express)
+const express = require('express')
 const router = express.Router()
 const axios = require('axios')
 
 router.get('/get-questions', (req, res) => {
+  if (!req.params) {
+    res.send('An error occurred: No parameters sent')
+  }
   const { categoryId, difficulty } = req.params
   return axios
     .get(
-      `https://opentdb.com/api.php?amount=10&category=${category}&type=multiple&difficulty=${difficulty}`
+      `https://opentdb.com/api.php?amount=10&category=${categoryId}&type=multiple&difficulty=${difficulty}`
     )
     .then(results => res.json(results.data.results))
     .catch(error => res.json(error))
